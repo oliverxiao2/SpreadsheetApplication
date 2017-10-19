e97f52be60effc12507af16fc34cc8cf668f6aa3
UAES\ext.ming.xiao <ext.ming.xiao@uaes.com>
2017-10-19 14:29:23 +0800

    Revert "migration from local"
    This reverts commit 2c5a0303522cdc5ae0aa954c26ce07e8a1c0cc0b.

--- views/scripts/chunk.js
+++
@@ -1,7 +0,0 @@
-function chunk (array) {
-    setTimeout(function (){
-        const task = array.shift();
-        task.handler(task.context, task.params);
-        if (array.length > 0) setTimeout(arguments.callee, 100);
-    });
-};
