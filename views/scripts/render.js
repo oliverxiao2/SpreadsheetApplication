6c527e73b3d3e9975aebae7cbadbe770651ec196
UAES\ext.ming.xiao <ext.ming.xiao@uaes.com>
2017-11-05 19:05:15 +0800

    注释掉tree组件

--- views/scripts/render.js
+++ views/scripts/render.js
@@ -1,7 +1,8 @@
 $(document).ready(() => {
+	/*
 	const Tree = require('./parts/tree/tree');
 	const resTree = new Tree('tree-container');
 	resTree.directory = 'E:\\Project'; // just for test
 	resTree.init();
-
+	*/
 });
