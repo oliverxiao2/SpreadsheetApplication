31fef5a6699635a9812ffa55d701386334e9b197
UAES\ext.ming.xiao <ext.ming.xiao@uaes.com>
2017-11-07 12:46:01 +0800

    增加menubar，方便调试

--- controllers/home/mainWindow.js
+++ controllers/home/mainWindow.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { app, shell, BrowserWindow } = require('electron');
+const { app, shell, BrowserWindow, Menu } = require('electron');
 const Common = require('../common/common');
 const UpdateHandler = require('../common/autoUpdate');
 
@@ -17,7 +17,7 @@ class MainWindow {
             show: false,
             frame: true,
             transparent: false,
-            autoHideMenuBar: true,
+            autoHideMenuBar: false,
             icon: path.join(__dirname, '../../../assets/icon.png'),
             titleBarStyle: 'hidden-inset',
             webPreferences: {
@@ -30,6 +30,55 @@ class MainWindow {
         });
 
         this.mainWindow.loadURL(`file://${path.join(__dirname, '/../../views/mainWindow.html')}`);
+
+        const template = [
+          {
+            label: 'Edit',
+            submenu: [
+              {role: 'undo'},
+              {role: 'redo'},
+              {type: 'separator'},
+              {role: 'cut'},
+              {role: 'copy'},
+              {role: 'paste'},
+              {role: 'pasteandmatchstyle'},
+              {role: 'delete'},
+              {role: 'selectall'}
+            ]
+          },
+          {
+            label: 'View',
+            submenu: [
+              {role: 'reload'},
+              {role: 'toggledevtools'},
+              {type: 'separator'},
+              {role: 'resetzoom'},
+              {role: 'zoomin'},
+              {role: 'zoomout'},
+              {type: 'separator'},
+              {role: 'togglefullscreen'}
+            ]
+          },
+          {
+            role: 'window',
+            submenu: [
+              {role: 'minimize'},
+              {role: 'close'}
+            ]
+          },
+          {
+            role: 'help',
+            submenu: [
+              {
+                label: 'Learn More',
+                click () { require('electron').shell.openExternal('https://sususususuke.github.io') }
+              }
+            ]
+          }
+        ]
+
+        const menu = Menu.buildFromTemplate(template);
+        Menu.setApplicationMenu(menu);
     }
 
     show() {
